3757599925886540d0106849765aa6e6b6352d65
michal_cfc <michal.jachimowski@gmail.com>
2022-11-30 12:45:56 +0100

    vercel deploy

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -3,9 +3,5 @@ import * as prismicNext from "@prismicio/next";
 import { prismicClient } from "../../prismicio";
 
 export default async function handler(req, res) {
-  const client = prismicClient({ req });
 
-  prismicNext.setPreviewData({ req, res });
-
-  await prismicNext.redirectToPreviewURL({ req, res, client });
 }
